dba25b94a20477dd3bd56dec24a9f3e90b9bd039
Bharat <bharatkalluri@protonmail.com>
2020-09-29 14:30:40 +0530

    Removed all flags and made error messages clearer (#15)

--- main.go
+++ main.go
@@ -2,68 +2,67 @@ package main
 
 import (
 	"fmt"
+	"github.com/BharatKalluri/spotifydl/src"
 	"os"
 	"strings"
 
-	spotifydl "github.com/BharatKalluri/spotifydl/src"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	var trackid string
-	var playlistid string
-	var albumid string
+	var trackID string
+	var playlistID string
+	var albumID string
 	var spotifyURL string
 
 	var rootCmd = &cobra.Command{
 		Use:   "spotifydl",
-		Short: "spotifydl is a awesome music downloader",
-		Long:  `Spotifydl lets you download albums and playlists and tags them for you.`,
+		Short: "spotifydl is a awesome music downloader!",
+		Long:  `spotifydl lets you download albums and playlists and tags them for you.`,
 		Run: func(cmd *cobra.Command, args []string) {
 
-			if len(spotifyURL) > 0 {
-				splitURL := strings.Split(spotifyURL, "/")
-
-				if len(splitURL) < 2 {
-					fmt.Println("Please enter the url copied from the spotify client")
-					os.Exit(1)
-				}
-
-				spotifyID := splitURL[len(splitURL)-1]
-				if strings.Contains(spotifyID, "?") {
-					fmt.Println("Please remove the part of the url after the question mark (?) and try again")
-					fmt.Println("For example, https://open.spotify.com/playlist/randomID?si=otherRandomID should just be https://open.spotify.com/playlist/randomID ")
-					os.Exit(1)
-				}
-
-				if strings.Contains(spotifyURL, "album") {
-					albumid = spotifyID
-				} else if strings.Contains(spotifyURL, "playlist") {
-					playlistid = spotifyID
-				} else if strings.Contains(spotifyURL, "track") {
-					trackid = spotifyID
-				}
+			if len(args) != 1 {
+				_ = cmd.Help()
 			}
 
-			if len(albumid) > 0 {
-				// Download album with the given album ID
-				spotifydl.DownloadAlbum(albumid)
-			} else if len(playlistid) > 0 {
-				// Download playlist with the given ID
-				spotifydl.DownloadPlaylist(playlistid)
-			} else if len(trackid) > 0 {
-				spotifydl.DownloadSong(trackid)
+			spotifyURL = args[0]
+
+			if len(spotifyURL) == 0 {
+				fmt.Println("=> Spotify URL required.")
+				_ = cmd.Help()
+				return
+			}
+
+			splitURL := strings.Split(spotifyURL, "/")
+
+			if len(splitURL) < 2 {
+				fmt.Println("=> Please enter the url copied from the spotify client.")
+				os.Exit(1)
+			}
+
+			spotifyID := splitURL[len(splitURL)-1]
+			if strings.Contains(spotifyID, "?") {
+				spotifyID = strings.Split(spotifyID, "?")[0]
+			}
+
+			if strings.Contains(spotifyURL, "album") {
+				albumID = spotifyID
+				spotifydl.DownloadAlbum(albumID)
+			} else if strings.Contains(spotifyURL, "playlist") {
+				playlistID = spotifyID
+				spotifydl.DownloadPlaylist(playlistID)
+			} else if strings.Contains(spotifyURL, "track") {
+				trackID = spotifyID
+				spotifydl.DownloadSong(trackID)
 			} else {
-				fmt.Println("Enter valid input.")
-				cmd.Help()
+				fmt.Println("=> Only Spotify Album/Playlist/Track URL's are supported.")
+				_ = cmd.Help()
 			}
+
 		},
 	}
 
-	rootCmd.Flags().StringVarP(&trackid, "trackid", "t", "", "Track ID found on spotify")
-	rootCmd.Flags().StringVarP(&playlistid, "playlistid", "p", "", "Playlist ID found on spotify")
-	rootCmd.Flags().StringVarP(&albumid, "albumid", "a", "", "Album ID found on spotify")
-	rootCmd.Flags().StringVarP(&spotifyURL, "spotifyurl", "u", "", "URL copied on spotify")
+	rootCmd.SetUsageTemplate(`spotifydl [spotify_url]`)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
