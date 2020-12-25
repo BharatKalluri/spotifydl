2b9d45fada90f116c6a2157d8004d66e9378eae4
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-12-25 15:02:43 +0530

    Upgraded dependencies

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/BharatKalluri/spotifydl
 go 1.13
 
 require (
-	github.com/bogem/id3v2 v1.1.1
-	github.com/spf13/cobra v0.0.5
-	github.com/zmb3/spotify v0.0.0-20191028153142-869e03dbd8b0
-	golang.org/x/oauth2 v0.0.0-20191122200657-5d9234df094c
-	google.golang.org/api v0.14.0
+	cloud.google.com/go v0.74.0 // indirect
+	github.com/bogem/id3v2 v1.2.0
+	github.com/spf13/cobra v1.1.1
+	github.com/zmb3/spotify v0.0.0-20200814173021-9bec46940cc0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	google.golang.org/api v0.36.0
+	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
 )
--- src/youtube.go
+++ src/youtube.go
@@ -1,11 +1,10 @@
 package spotifydl
 
 import (
-	"log"
-	"net/http"
-
-	"google.golang.org/api/googleapi/transport"
+	"context"
+	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
+	"log"
 )
 
 // Please do not misuse :)
@@ -13,15 +12,12 @@ const developerKey = "AIzaSyDQn4VAc4MzrKOjo2sv5ucmKsQUIfKFaSE"
 
 // GetYoutubeIds takes the query as string and returns the search results video ID's
 func GetYoutubeIds(songName string) string {
-	client := &http.Client{
-		Transport: &transport.APIKey{Key: developerKey},
-	}
-	service, err := youtube.New(client)
+	service, err := youtube.NewService(context.TODO(), option.WithAPIKey(developerKey))
 	if err != nil {
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
 	// Video category ID 10 is for music videos
-	call := service.Search.List("id,snippet").Q(songName).VideoCategoryId("10").Type("video")
+	call := service.Search.List([]string{"id", "snippet"}).Q(songName).VideoCategoryId("10").Type("video")
 	response, err := call.Do()
 	if err != nil {
 		log.Fatalf("Error making search API call: %v", err)
