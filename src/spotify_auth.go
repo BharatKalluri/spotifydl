4c508218d36d719baabe3adbe92d87f4c1c67291
Vladimir Kiselev <first3395@gmail.com>
2022-03-25 04:29:42 +0300

    remove hardcoded tokens

--- src/spotify_auth.go
+++ src/spotify_auth.go
@@ -6,10 +6,7 @@ import (
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2/clientcredentials"
 	"log"
-
-	// transport is used as dependency for youtube API
-	_ "google.golang.org/api/googleapi/transport"
-	_ "google.golang.org/api/youtube/v3"
+	"os"
 )
 
 // UserData is a struct to hold all variables
@@ -25,8 +22,8 @@ func InitAuth() *spotify.Client {
 	ctx := context.Background()
 	// Please do not misuse :)
 	config := &clientcredentials.Config{
-		ClientID:     "07d728d8751646219ab0532ca21ba982", // TODO: os.Getenv("SPOTIFY_ID"),
-		ClientSecret: "6ad82c4fd7cc498fbf738ea08f4135d3", // TODO: os.Getenv("SPOTIFY_SECRET"),
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
 		TokenURL:     spotifyauth.TokenURL,
 	}
 	token, err := config.Token(context.Background())
